d5e7294bce970895252563aac411939b80d13b50
Scott Milton <scottmilton1@gmail.com>
2020-08-18 18:17:29 -0400

    Add docstring project description stub to module.

---
+++ twosquare.py
@@ -0,0 +1,8 @@
+"""An implementation of the two-square cipher.
+
+This program encypts and decrypts messages using the two-square cipher,
+aka double Playfair.
+
+It can also be used as a module. 
+
+"""
