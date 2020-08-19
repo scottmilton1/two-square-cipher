599f3b3e849b8f11e05ff4ec1aff1b90077c9987
Scott Milton <scottmilton1@gmail.com>
2020-08-19 16:32:50 -0400

    Add comment TODOs for planned features to main program. Expand implementation details in module-level docstring description.

--- twosquare.py
+++ twosquare.py
@@ -3,14 +3,46 @@
 """An implementation of the two-square cipher.
 
 This program encypts and decrypts messages using the two-square cipher,
-aka double Playfair.
+aka double Playfair.There are two variations of the cipher, vertical
+and horizontal, depending on how the two Playfair tables are arranged.
+This implementation uses the first variation, so the two tables are
+placed vertically.
 
-It can also be used as a module. 
+The functionality of this program can also be used as a module.
 
 """
 
 def __main__():
+
     pass
+
+    # TODOs:
+
+    # display program title and brief description
+
+    # prompt user for mode - encrypt or decrypt
+    
+    # prompt user for first key
+
+    # validate key
+
+    # prompt user for second key
+
+    # validate key
+
+    # create first table with first key
+
+    # create second table with second key
+
+    # display the tables to the console for viewing - optional functionality
+
+    # prompt the user for message (or text file) to encrypt / decrypt
+
+    # perform encoding or decoding of message (or text file)
+
+    # display message to confirm operation success or failure
+
+    # print out the modified text to the console - optional functionality
 
 if __name__ == '__main__':
     __main__()
