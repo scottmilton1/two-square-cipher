327fb6191abff3ea4c3b25e95032a38fc9318791
Scott Milton <scottmilton1@gmail.com>
2020-08-29 22:16:38 -0400

    Implement __init__ and __repr__ methods for all custom error classes in twosquare.py.

--- twosquare/twosquare.py
+++ twosquare/twosquare.py
@@ -22,32 +22,95 @@ from typing import Union
 Row = List[str]
 Table = List[Row]
 
+# move these to exceptions.py or errors.py
 class FooBarError(Exception):
     """Raised when something really bad happens.
 
+    Attributes:
+        message -- explanation of the error
+        subtext -- humorous subtext message to accompany the error info
+
     """
-    pass
+
+    def __init__(self, message = 'Raised when something really bad happens.'):
+        self.message = message
+        
+        self.subtext = '\nI do not like green eggs and ham. ' + \
+                       'This should never happen, but if it does, ' + \
+                       'it is an unforeseen error. ' + \
+                       "It's okay to panic!"
+        
+        self.message += self.subtext
+        
+        super().__init__(self.message)
+
+    def __repr__(self):
+        return f'{self.message!r}'     
 
 class TypeMismatchError(TypeError):
     """Raised when someone tries to put a square peg in a round hole.
 
+    Attributes:
+        message -- explanation of the error
+
     """
 
-    pass
+    def __init__(self, message):
+        self.message = message if len(message) > 0 else \
+            'Raised when someone tries to put a square peg in a round hole.'        
+##        self.message = \
+##            'Raised when someone tries to put a square peg in a round hole.'
+        
+        super().__init__(self.message)
+
+    def __repr__(self):
+        return f'{self.message!r}'
     
 class StakesTooHighError(IndexError):
     """Raised when the stakes are just too dang high!
 
+    Attributes:
+        message -- explanation of the error
+
     """
 
-    pass 
+    def __init__(self, message):
+        self.message = message if len(message) > 0 else \
+            'Raised when the stakes are just too dang high!'     
+##        self.message = 'Raised when the stakes are just too dang high!'
+        
+        super().__init__(self.message)
+
+    def __repr__(self):
+        return f'{self.message!r}'
 
 class BadValueError(ValueError):
     """Raised when the value is flagrantly and absolutely invalid.
 
+    Attributes:
+        message -- explanation of the error
+        subtext -- humorous subtext message to accompany the error info
+
     """
 
-    pass
+    def __init__(self, message = ''):
+        self.message = message
+        
+        if len(message) == 0: # default message
+            self.message =  \
+                'Raised when the value is flagrantly and absolutely invalid.'
+            
+        self.subtext = '\nYou have attempted to exceed the limits ' + \
+                        'of reality imposed by the Architect. ' + \
+                        'Neo could bend the Matrix ' + \
+                        'to his will, but can you?'
+        
+        self.message += self.subtext
+        
+        super().__init__(self.message)
+
+    def __repr__(self):
+        return f'{self.message!r}'
 
 def create_table(key: str) -> Union[Table, bool]: # return either Table or False
     """Create a Playfair table.
@@ -384,11 +447,11 @@ def encrypt(plaintext: str, key1: str, key2: str) -> Union[str, bool]:
         return False
 
     except FooBarError as err:
-        print('I do not like green eggs and ham.')
-        print('This should never happen, but if it does, ' +
-            'it is an unforeseen error.')
-        print("It's okay to panic!")
-        print(type(err))
+##        print('I do not like green eggs and ham.')
+##        print('This should never happen, but if it does, ' +
+##            'it is an unforeseen error.')
+##        print("It's okay to panic!")
+##        print(type(err))
         print(err)
         raise
 
@@ -644,9 +707,9 @@ def validate_message(message: str, mode: str = 'encrypt') -> bool:
 
     except ValueError as err:
         print(err)
-        print('You have attempted to exceed the limits of reality')
-        print('imposed by the Architect. Neo could bend the Matrix')
-        print('to his will, but can you?')
+##        print('You have attempted to exceed the limits of reality')
+##        print('imposed by the Architect. Neo could bend the Matrix')
+##        print('to his will, but can you?')
         return False
 
     except TypeError as err:
@@ -757,6 +820,31 @@ def __main__():
 
     The functionality of this implementation can also be used as a module.
     """
+
+##    # print(FooBarError())
+##
+##    try:
+##        raise FooBarError('Oh la la!')
+##
+##    except FooBarError as err:
+##        print(err)
+##
+##    print()
+##
+##    raise FooBarError()
+##    
+##
+##   # print(BadValueError())
+##
+##    try:
+##        raise BadValueError('Jeepers creepers, Batman!')
+##
+##    except BadValueError as err:
+##        print(err)
+##
+##    print()
+##
+##    raise BadValueError()
 
     # display program title and brief description
     name: str = "twosquare"
